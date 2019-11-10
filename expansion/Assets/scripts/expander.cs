93f0395f4f7a8359c5da8a1ece8e415c88548aed
Sander <sander.pleym@gmail.com>
2019-11-10 22:48:12 +0100

    first unity project commit

---
+++ expansion/Assets/scripts/expander.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class expander : MonoBehaviour
+{
+    public Transform currentPos;
+    public GameObject spawnee;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown("a")) {
+        Vector3 xIncrease = new Vector3(1, 0, 0);
+        currentPos.position += xIncrease;
+        Instantiate(spawnee, currentPos);
+        
+        }
+    }
+}
