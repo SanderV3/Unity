175bebadc6bc59a8d43ffc98d6179ee42e9289f8
Sander <sander.pleym@gmail.com>
2019-11-10 22:55:47 +0100

    expanding cubes

---
+++ expandingCubes/Assets/expandingCubes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class expandingCubes : MonoBehaviour
+{
+    public GameObject expandingHomie;
+    public float distance;
+    public Transform currentPos;
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if(Input.GetKeyDown("a") && cubeScript.crashed == false){
+            
+            Instantiate(expandingHomie, currentPos.position + (transform.right * distance), transform.rotation);
+            Instantiate(expandingHomie, currentPos.position + (transform.right * -distance), transform.rotation);
+            distance += 1;
+            
+        }
+        
+    }
+
+    
+}
+
+
+    
